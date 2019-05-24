3f4977d33b2de427649ef3ede20eb2414d78ddd3
cxp <2256843428@qq.com>
2019-05-24 18:02:12 +0800

    首页图片，新闻

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,7 @@ Page({
     // 轮播图图片路径
     newsImaUrl: [],
     // 标题栏文字
-    btList: ["现场督导", "现场督导", "现场督导", "现场督导", "现场督导", "现场督导"],
+    btList: [],
     // 控制标题栏选中样式
     isActive: 2,
     // 左侧图标属性值
@@ -26,24 +26,45 @@ Page({
     app.$kwz.ajax.ajaxUrl({
       url: '/jc_mobile/open/getYkXtsz',
       type: 'POST',
+      page: this,
       success (data) {
         if (data && '200' == data.statusCode) {
           let ykpic = data.datas.YKPIC
+          let ykinfo = data.datas.YKINFO;
           // 首页图片
           if (ykpic && ykpic.length > 0) {
             for(let i =0;i<ykpic.length;i++) {
               app.$kwz.cacheAttach({
                 url: '/index/visit/doDownload?F_ID=' + ykpic[i].F_ID,
+                page: this,
                 success (filepath) {
                   if (filepath.statusCode === 200) {
-                    let newsImaUrl = that.data.newsImaUrl
+                    let newsImaUrl = this.data.newsImaUrl
                     newsImaUrl.push(filepath.tempFilePath)
-                    that.setData({newsImaUrl})
+                    this.setData({newsImaUrl})
                   }
                 }
               })
             }
           }
+          // 首页新闻
+          if (ykinfo && ykinfo.length > 0) {
+            for (let i = 0; i < ykinfo.length; i++) {
+              this.data.btList.push(ykinfo[i].YWMC)
+              this.data.newsBtList.push(ykinfo[i].CONTENT)
+            }
+          }
+          // 因组织架构图并不能从后端配出来，顾前端手动添加
+          this.data.btList.push("组织架构图")
+          this.data.newsBtList.push([{
+            TITLE: "组织架构图",
+            CONTENT_ID: "组织架构图"
+          }])
+          this.setData({
+            btList: this.data.btList,
+            newsBtList: this.data.newsBtList
+          })
+          console.log(this.data.newsBtList)
         }
       }
     })

1ae10e8d941cefdda42494b117ed0bd1446c9356
chenxp <2256843428@qq.com>
2019-07-23 15:47:38 +0800

    组织架构图 查看督学&&查看学校 bug修复

--- pages/zzjgt/zzjgt.js
+++ pages/zzjgt/zzjgt.js
@@ -1,39 +1,39 @@
 // pages/zzjgt/zzjgt.js
 const app = getApp()
 Page({
-  data: {
-    // 页面数据
-    data: [],
-    // 查看督学显示隐藏
-    ckdxShow: false,
-    // 查看督学/学校数据
-    dxxxData: {}
-  },
-  onReady: function () {
-    this.initData()
-  },
-  // 初始化数据
-  initData() {
-    app.$kwz.ajax.ajaxUrl({
-      url: '/dd_zrqgl/open/doPageListZzjgt',
-      type: 'POST',
-      page: this,
-      success(data) {
-        this.setData({ 
-          data: data.datas
+    data: {
+        // 页面数据
+        data: [],
+        // 查看督学显示隐藏
+        ckdxShow: false,
+        // 查看督学/学校数据
+        dxxxData: {}
+    },
+    onReady: function() {
+        this.initData()
+    },
+    // 初始化数据
+    initData() {
+        app.$kwz.ajax.ajaxUrl({
+            url: '/dd_zrqgl/open/doPageListZzjgt',
+            type: 'POST',
+            page: this,
+            success(data) {
+                this.setData({
+                    data: data.datas
+                })
+            }
         })
-      }
-    })
-  },
-  // 查看督学/学校 关闭打开
-  oneCkdx(e) {
-    this.setData({ 
-      ckdxShow: !this.data.ckdxShow,
-    })
-    if (e.target.dataset.list){
-      this.setData({
-        dxxxData:  e.target.dataset.list
-      })
-    }
-  },
+    },
+    // 查看督学/学校 关闭打开
+    oneCkdx({target}) {
+        this.setData({
+            ckdxShow: !this.data.ckdxShow,
+        })
+        if (target.dataset.list) {
+            this.setData({
+                dxxxData: target.dataset.list
+            })
+        }
+    },
 })

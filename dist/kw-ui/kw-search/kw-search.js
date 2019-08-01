6e268c400a8aea513bfd3fc3a677af4e7a35ffdd
chenxp <2256843428@qq.com>
2019-08-01 15:17:03 +0800

    解决 ios端 kw-search组件bug

--- dist/kw-ui/kw-search/kw-search.js
+++ dist/kw-ui/kw-search/kw-search.js
@@ -1,65 +1,66 @@
 // dist/kw-ui/kw-search/kw-search.js
 Component({
-  options:{
-    styleIsolation: "apply-shared" // 表示页面wxss样式将影响到自定义组件，但自定义组件wxss中指定的样式不会影响页面
-  },
-  externalClasses: ['kw-class'],  // 接受外部传入的样式类
-  properties: {
-    value: { // 搜索框值
-      value: '',
-      type: String
+    options: {
+        styleIsolation: "apply-shared" // 表示页面wxss样式将影响到自定义组件，但自定义组件wxss中指定的样式不会影响页面
     },
-    placeholder: { // 搜索框占位符
-      type: String,
-      value: "请输入"
+    externalClasses: ['kw-class'], // 接受外部传入的样式类
+    properties: {
+        value: { // 搜索框值
+            value: '',
+            type: String
+        },
+        placeholder: { // 搜索框占位符
+            type: String,
+            value: "请输入"
+        },
+        // 是否要输入框
+        isInput: {
+            type: Boolean,
+            value: true
+        }
     },
-    // 是否要输入框
-    isInput: {
-      type: Boolean,
-      value: true
-    }
-  },
-  data: {
-    ifFocus: false,// 是否点击输入框
-    inputShow: true
-  },
-  methods: {
-    //点击输入框
-    tapFocus() {
-      this.setData({
-        ifFocus: true,
-      })
-      if (!this.data.isInput) {
-        this.setData({
-          inputShow: false
-        })
-      }
-    },
-    // 输入搜索时改变量
-    inputChange(e) {
-      // this.setData({
-      //   value: e.detail.value
-      // })
-      this.data.value = e.detail.value
-      return e.detail.value
-    },
-    // 取消
-    cancel() {
-      this.setData({
-        ifFocus: false,
+    data: {
+        ifFocus: false, // 是否点击输入框
         inputShow: true
-      })
-      this.triggerEvent("cancel")
     },
-    // 确定 callback:输入框输入内容
-    confirm() {
-      this.setData({
-        ifFocus: false,
-        inputShow: true
-      })
-      this.triggerEvent("confirm", {
-        value: this.data.value
-      })
+    methods: {
+        //点击输入框
+        tapFocus() {
+            this.setData({
+                ifFocus: true,
+            })
+            if (!this.data.isInput) {
+                this.setData({
+                    inputShow: false
+                })
+            }
+        },
+        // 输入搜索时改变量
+        inputChange(e) {
+            // this.setData({
+            //   value: e.detail.value
+            // })
+            this.data.value = e.detail.value
+            return e.detail.value
+        },
+        // 取消
+        cancel() {
+            this.setData({
+                ifFocus: false,
+                inputShow: true
+            })
+            this.triggerEvent("cancel")
+        },
+        // 确定 callback:输入框输入内容
+        confirm() {
+            this.setData({
+                ifFocus: false,
+                inputShow: true
+            })
+            this.triggerEvent("confirm", {
+                value: this.data.value
+            })
+        },
+        catchtouchmove() {}
     }
-  }
 })
